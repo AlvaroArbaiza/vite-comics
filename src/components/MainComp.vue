29db449699a9713e840fea1f29be720f8a0e5bb4
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-03 17:35:25 +0200

    Completamento MainComp

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -13,8 +13,26 @@ export default {
 
 <template>
     <main>
-        <section id="contents"></section>
-        <section id="cards"></section>
+        <!-- contents -->
+        <section id="contents">
+            <div class="container">
+
+                <h1> &#45;&#45;&#62; Contents goes here &#60;&#45;&#45; </h1>
+            </div>
+        </section>
+
+        <!-- cards -->
+        <section id="cards">
+            <div class="container">
+
+                <div class="dcCard" v-for="(element, index) in store.dcCards" :key="index">
+                    <div class="img">
+                        <img :src="`/img/${element.url}`" :alt="element.name">
+                    </div>
+                    <h3>{{ element.name }}</h3>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -25,12 +43,66 @@ export default {
     margin: 0;
 }
 
+.container {
+    width: 80%;
+    margin: 0 auto;
+}
+
+img {
+    max-width: 100%;
+}
+
+
 main {
-
     width: 100%;
-    min-height: 200px;
-    background-color: #1c1c1c;
 
+    #contents {
+        min-height: 200px;
+        background-color: #1c1c1c;
+        display: flex;
+        align-items: center;
+
+        h1 {
+            color: #fff;
+        }
+    }
+
+    #cards {
+        height: 200px;
+        background-color: #0282f9;
+        display: flex;
+        align-items: center;
+
+        .container {
+            width: 75%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .dcCard {
+                display: flex;
+                align-items: center;
+                flex-basis: calc(100% / 5);
+
+                .img {
+                    flex-basis: 20%;
+
+                    img {
+                        max-height: 95px;
+                    }
+                }
+
+                h3 {
+                    margin-left: 10px;
+                    color: #fff;
+                    font-weight: 400;
+                    font-size: 0.8rem;
+                    text-transform: uppercase;
+                }
+
+            }
+        }
+    }
 
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -81,5 +81,27 @@ export const store = reactive({
                 "DC Power Visa"
             ]
         }
+    ],
+    dcCards: [
+        {
+            name: "digital comics",
+            url: "buy-comics-digital-comics.png"
+        },
+        {
+            name: "dc merchandise",
+            url: "buy-comics-merchandise.png"
+        },
+        {
+            name: "subscription",
+            url: "buy-comics-subscriptions.png"
+        },
+        {
+            name: "comic shop locator",
+            url: "buy-comics-shop-locator.png"
+        },
+        {
+            name: "dc power visa",
+            url: "buy-dc-power-visa.svg"
+        }
     ]
 })
