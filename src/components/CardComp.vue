677d69bef772fdad9d0d860901aa6d6bbae9e8f3
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-04 15:36:39 +0200

    Inserimento cards tramite props

---
+++ src/components/CardComp.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "CardComp",
+    props: {
+        dettagliProdotto: Object
+    }
+}
+</script>
+
+<template>
+    <!-- Series -->
+    <div class="series">
+
+        <img :src="dettagliProdotto.thumb" :alt="dettagliProdotto.series">
+        <h4>{{ dettagliProdotto.series }}</h4>
+    </div>
+</template>
+<style lang="scss" scoped></style>
